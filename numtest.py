4131a2324ceacc71cd756e59b61467c76ac4121f
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2012-12-06 13:41:39 +0100

    added doctest NUMBER directive

--- numtest.py
+++ numtest.py
@@ -1,12 +1,36 @@
 
 import doctest
 
-class NumTestOutputChecker(doctest.OutputChecker):
+_doctest_OutputChecker = doctest.OutputChecker
+
+class NumTestOutputChecker(_doctest_OutputChecker):
     def check_output(self, want, got, optionflags):
-        super_check_output = super(NumTestOutputChecker, self).check_output
+        super_check_output = _doctest_OutputChecker.check_output
         return super_check_output(self, want, got, optionflags)
     def output_difference(self, example, got, optionflags):
-        super_output_difference = super(NumTestOutputChecker, self).output_difference
+        super_output_difference = _doctest_OutputChecker.output_difference
         return super_output_difference(self, example, got, optionflags)
 
 doctest.OutputChecker = NumTestOutputChecker
+
+NUMBER = doctest.register_optionflag("NUMBER")
+doctest.NUMBER = NUMBER
+doctest.__all__.append("NUMBER")
+
+def test():
+    """
+    >>> import doctest
+    >>> doctest.OutputChecker == NumTestOutputChecker
+    True
+    >>> _ = doctest.NUMBER
+    >>> from doctest import *
+    >>> NUMBER == doctest.NUMBER
+    True
+    >>> OutputChecker == NumTestOutputChecker
+    True
+    """
+    doctest.testmod()
+
+if __name__ == "__main__":
+    test()
+
