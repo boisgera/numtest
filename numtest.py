aa02b71b11befb5ddfe34539fe93847771b87b61
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2012-12-14 18:04:44 +0100

    support for array

--- numtest.py
+++ numtest.py
@@ -4,6 +4,8 @@ import doctest
 import re
 import sys
 
+import numpy
+
 _doctest_OutputChecker = doctest.OutputChecker
 
 _float = r"""[-+]?
@@ -16,10 +18,10 @@ _float = r"""[-+]?
           """
 _float = re.compile(_float, re.VERBOSE)
 
-def exponent(number_repr):
-    match = _float.match(number_repr)
-    if not match or match.group() != number_repr:
-        raise ValueError("invalid float syntax {0!r}".format(number_repr))
+def exponent(number):
+    match = _float.match(number)
+    if not match or match.group() != number:
+        raise ValueError("invalid float syntax {0!r}".format(number))
     else:
         ndigits  = len(match.group("digits") or "")
         exponent = int(match.group("exponent") or 0)
@@ -30,18 +32,61 @@ doctest.NUMBER = NUMBER
 doctest.__all__.append("NUMBER")
 doctest.COMPARISON_FLAGS = doctest.COMPARISON_FLAGS | NUMBER
 
-def number_match(target, candidate):
-    threshold = 0.5 * 10 ** (exponent(target))
-    return abs(float(target) - float(candidate)) < threshold    
-    
+def number_match(want, got):
+    threshold = 0.5 * 10 ** (exponent(want))
+    match = abs(float(want) - float(got)) < threshold    
+    if not match:
+        raise RuntimeError(want, got)
+    else:
+        return True    
+
+def array_match(want, got):
+    try: 
+        want_array = eval_array(want)
+        got_array = eval_array(got)
+    except SyntaxError:
+        return False
+    if numpy.shape(want_array) != numpy.shape(got_array):
+        return False
+    else:
+        want_array = numpy.ravel(want_array)
+        got_array = numpy.ravel(got_array)
+        indices = range(len(want_array))
+        return all(number_match(want_array[i], got_array[i]) for i in indices)
+
+def eval_array(numbers):
+    """
+Read a scalar or array of numbers representation as an array of strings:
+
+    >>> print eval_array("1.34e-7")
+    1.34e-7
+    >>> print eval_array("42")
+    42
+    >>> print eval_array("[42]")
+    ['42']
+    >>> print eval_array("[1, 2, 3]")
+    ['1' '2' '3']
+    >>> print eval_array("[[0.1, 2.00], [1e-2, 3.14], [6.78e7, 0.001e-6]]")
+    [['0.1' '2.00']
+     ['1e-2' '3.14']
+     ['6.78e7' '0.001e-6']]
+    """
+    _numbers = eval(numbers) # ouch.
+    shape = numpy.shape(_numbers)
+    if not shape:
+        return numpy.array(numbers)
+    else:
+        numbers = [m.group(0) for m in re.finditer(_float, numbers)]
+        return numpy.reshape(numbers, shape)
+
 class NumTestOutputChecker(_doctest_OutputChecker):
     def check_output(self, want, got, optionflags):
         if optionflags & NUMBER:
-            try:
-                target, candidate = want[:-1], got[:-1]
-                return number_match(target, candidate)
-            except ValueError:
-                return False
+            if want.endswith("\n"):
+                want = want[:-1]
+            if got.endswith("\n"):
+                got = got[:-1]
+            return array_match(want, got)
         else:
             super_check_output = _doctest_OutputChecker.check_output
             return super_check_output(self, want, got, optionflags)
@@ -65,7 +110,8 @@ Test the succes of doctest monkey-patching
     >>> OutputChecker == NumTestOutputChecker
     True
 
-Test the directive:
+Test the directive on scalars:
+
     >>> import math
     >>> math.pi
     3.141592653589793
@@ -79,11 +125,19 @@ Test the directive:
     3.14
     >>> math.pi # doctest: +NUMBER
     3
-
     >>> 951 # doctest: +NUMBER
     1e3
     >>> 1049 # doctest: +NUMBER
     1e3
+
+Test the directive on arrays:
+
+    >>> [3.1415, 0.097, 13.1, 7, 8.22222e5, 0.598e-2] # doctest: +NUMBER
+    [3.14, 0.1, 13, 7, 8.22e5, 6.0e-3]
+    >>> [[0.333, 0.667], [0.999, 1.333]] # doctest: +NUMBER
+    [[0.33, 0.667], [0.999, 1.333]]
+    >>> [[[0.101]]] # doctest: +NUMBER
+    [[[0.1]]]
     """
     doctest.testmod()
 
