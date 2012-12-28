1102c54dc9b212539752cde85d9f9322399ec957
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2012-12-28 16:22:32 +0100

    solved matching array bugs

--- numtest.py
+++ numtest.py
@@ -5,13 +5,14 @@
 import doctest
 import re
 import sys
-
+import StringIO
+import tokenize
 # Third-Party Libraries
 import numpy
 
 
 __author__ = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>"
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __license__ = "MIT License"
 
 
@@ -25,10 +26,10 @@ _float = r"""[-+]?
              )
              (?:[Ee](?P<exponent>[+-]?\d+))?
           """
-_float = re.compile(_float, re.VERBOSE)
+_float_pattern = re.compile(_float, re.VERBOSE)
 
 def exponent(number):
-    match = _float.match(number)
+    match = _float_pattern.match(number)
     if not match or match.group() != number:
         raise ValueError("invalid float syntax {0!r}".format(number))
     else:
@@ -51,8 +52,8 @@ def number_match(want, got):
 
 def array_match(want, got):
     try: 
-        want_array = eval_array(want)
-        got_array = eval_array(got)
+        want_array = parse_numbers(want)
+        got_array  = parse_numbers(got)
     except SyntaxError:
         return False
     if numpy.shape(want_array) != numpy.shape(got_array):
@@ -63,30 +64,69 @@ def array_match(want, got):
         indices = range(len(want_array))
         return all(number_match(want_array[i], got_array[i]) for i in indices)
 
-def eval_array(numbers):
+def parse_numbers(text):
     """
-Read a scalar or array of numbers representation as an array of strings:
+Parse text that represent numbers.
 
-    >>> print eval_array("1.34e-7")
-    1.34e-7
-    >>> print eval_array("42")
-    42
-    >>> print eval_array("[42]")
+The text can be a single number or a list of numbers that may be nested.
+
+    >>> parse_numbers("1.34e-7")
+    '1.34e-7'
+    >>> parse_numbers("42")
+    '42'
+    >>> parse_numbers("[42]")
     ['42']
-    >>> print eval_array("[1, 2, 3]")
-    ['1' '2' '3']
-    >>> print eval_array("[[0.1, 2.00], [1e-2, 3.14], [6.78e7, 0.001e-6]]")
-    [['0.1' '2.00']
-     ['1e-2' '3.14']
-     ['6.78e7' '0.001e-6']]
+    >>> parse_numbers("[1, 2, 3]")
+    ['1', '2', '3']
+    >>> parse_numbers("[[0.1, 2.00], [1e-2, 3.14], [6.78e7, 0.001e-6]]")
+    [['0.1', '2.00'], ['1e-2', '3.14'], ['6.78e7', '0.001e-6']]
+    >>> parse_numbers("array([0, 1, 2], uint8)")
+    ['0', '1', '2']
     """
-    _numbers = eval(numbers) # ouch.
-    shape = numpy.shape(_numbers)
-    if not shape:
-        return numpy.array(numbers)
+    text_wrapper = StringIO.StringIO(text).readline
+    tokens = list(tokenize.generate_tokens(text_wrapper))
+
+    def valid(token):
+        type_, text, _, _, _ = token
+        if type_ in [tokenize.NUMBER, tokenize.NL, tokenize.ENDMARKER]:
+            return True
+        elif type_ == tokenize.OP and text in ["[", "]", ","]:
+            return True
+        else:
+            return False
+
+    while tokens and not valid(tokens[0]):
+        tokens.pop(0)
+    if not tokens:
+        return None
+
+    stack = [[]]
+    fold = lambda: stack[-2].append(stack.pop())
+    insert = lambda: stack.append([])
+    push = lambda item: stack[-1].append(item)
+
+    for token in tokens:
+        type_, text, _, _, _ = token
+        if type_ == tokenize.OP:
+            if text == "[":
+                insert()        
+            elif text == "]":
+                fold()
+            elif text == ",":
+                pass
+            else:
+                break
+        elif type_ == tokenize.NUMBER:
+            push(text)
+        elif type_ == tokenize.NL:
+            pass
+        else:
+            break
+
+    if len(stack) != 1:
+        raise SyntaxError("invalid syntax {0!r}".format(text))
     else:
-        numbers = [m.group(0) for m in re.finditer(_float, numbers)]
-        return numpy.reshape(numbers, shape)
+        return stack[0][0]
 
 class NumTestOutputChecker(_doctest_OutputChecker):
     def check_output(self, want, got, optionflags):
@@ -139,7 +179,7 @@ Test the directive on scalars:
     >>> 1049 # doctest: +NUMBER
     1e3
 
-Test the directive on arrays:
+Test the directive on lists:
 
     >>> [3.1415, 0.097, 13.1, 7, 8.22222e5, 0.598e-2] # doctest: +NUMBER
     [3.14, 0.1, 13, 7, 8.22e5, 6.0e-3]
@@ -147,6 +187,16 @@ Test the directive on arrays:
     [[0.33, 0.667], [0.999, 1.333]]
     >>> [[[0.101]]] # doctest: +NUMBER
     [[[0.1]]]
+
+Test the directive on arrays:
+
+    >>> from numpy import array
+    >>> array([]) # doctest: +NUMBER
+    []
+    >>> array([1.0]) # doctest: +NUMBER
+    [1]
+    >>> array([[1.0, 2.0], [3.0, 4.0]]) # doctest: +NUMBER
+    [[1, 2], [3, 4]]
     """
     doctest.testmod()
 
