486e5ab4d35040a50a0a6a0314ab5cb02039eb83
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-02-14 19:54:43 +0100

    version 1.2.1

--- about_numtest.py
+++ about_numtest.py
@@ -2,7 +2,7 @@
 
 __project__ = "numtest"
 __author__  = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>"
-__version__ = "1.2.0"
+__version__ = "1.2.1"
 __license__ = "MIT License"
 __url__     = "https://github.com/boisgera/numtest"
 
