23cf6174f0bff237d61c3225ac1b4d6456b02351
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-02-14 19:53:51 +0100

    added project name info

---
+++ about_numtest.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+__project__ = "numtest"
+__author__  = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>"
+__version__ = "1.2.0"
+__license__ = "MIT License"
+__url__     = "https://github.com/boisgera/numtest"
+
