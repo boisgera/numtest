c56871b63db4af78af35e159bed11e724c71e7c9
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-02-15 15:22:30 +0100

    properly exported metadata

--- about_numtest.py
+++ about_numtest.py
@@ -6,3 +6,6 @@ __version__ = "1.2.1"
 __license__ = "MIT License"
 __url__     = "https://github.com/boisgera/numtest"
 
+export = "project author version license url".split()
+__all__ = ["__" + name + "__" for name in export]
+
