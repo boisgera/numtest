d6355ec5ebfe4a3ef2bf982295eee7fc99b212b3
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2014-11-16 00:52:24 +0100

    added summary

--- numtest/about_numtest.py
+++ numtest/about_numtest.py
@@ -6,7 +6,7 @@ metadata = dict(
   __license__     = "MIT License",
   __author__      = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
   __url__         = "https://github.com/boisgera/numtest",
-  __summary__     = None,
+  __summary__     = "Numeric Doctest",
   __readme__      = None,
   __classifiers__ = None,
 )
