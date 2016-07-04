57c8a728bc81baabe8898136eb82a7228e9ea081
Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>
2016-07-04 09:38:47 +0200

    clean up dev dependencies

--- numtest/about_numtest.py
+++ numtest/about_numtest.py
@@ -2,7 +2,7 @@
 
 metadata = dict(
   __name__        = "numtest",
-  __version__     = "2.0.1",
+  __version__     = "2.0.3",
   __license__     = "MIT License",
   __author__      = u"Sébastien Boisgérault <Sebastien.Boisgerault@mines-paristech.fr>",
   __url__         = "https://github.com/boisgera/numtest",
